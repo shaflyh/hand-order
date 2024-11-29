6262ce519b33d8dad707b3e6670aac9a204aa58f
shaflyh <shafly2000@gmail.com>
2024-11-29 08:31:55 +0700

    fix: error in mapper

--- src/main/java/com/hand/demo/infra/mapper/InvCountHeaderMapper.java
+++ src/main/java/com/hand/demo/infra/mapper/InvCountHeaderMapper.java
@@ -2,7 +2,9 @@ package com.hand.demo.infra.mapper;
 
 import com.hand.demo.domain.entity.InvCountHeader;
 import io.choerodon.mybatis.common.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface InvCountHeaderMapper extends BaseMapper<InvCountHeader> {
 
     int updateByCountHeaderKey(InvCountHeader invCountHeader);
